caba871b124cf9a41add0f04b58a0d9b3183eaba
SakuHeli0s <mariabayanescobar@gmail.com>
2026-03-20 13:32:36 +0100

    Proyecto terminado.

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
 //CARGAR HTML AL JS
 
 const productList = document.getElementById('product-list')
+
+
+//ARRAY DE PRODUCTOS
+
+const productos = [
+    { nombre: "Camiseta", categoria: "ropa", precio: 19.99, disponible: true },
+    { nombre: "Auriculares", categoria: "electrónica", precio: 49.99, disponible: false },
+    { nombre: "Zapatillas", categoria: "ropa", precio: 89.99, disponible: true },
+    { nombre: "Teclado", categoria: "electrónica", precio: 35.00, disponible: true },
+    { nombre: "Chaqueta", categoria: "ropa", precio: 120.00, disponible: false },
+    { nombre: "Ratón", categoria: "electrónica", precio: 25.00, disponible: true }
+]
+
+const productosDisponibles = productos.filter(productos => productos.disponible)
+
+
+//TARJETAS JS
+
+productosDisponibles.forEach(producto => {
+    const cardTHTML = `
+    <article class="product-card">
+    <img src="https://picsum.photos/500/300" alt="${producto.nombre}">
+    <h3>${producto.nombre}</h3>
+    <p>${producto.categoria}</p>
+    <span>${producto.precio}€</span>
+    <button>Añadir al carrito</button>
+    </article>
+    `
+
+    productList.insertAdjacentHTML('beforeend', cardTHTML)
+})
